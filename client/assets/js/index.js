d7291cd007a20884ba76b2a3840430f51ec115e3
Danielle Man <daniman0808@gmail.com>
2015-03-04 12:22:35 -0500

    removed handlebars for now

--- client/assets/js/index.js
+++ client/assets/js/index.js
@@ -33,8 +33,3 @@ $(document).ready(function() {
     }
 
 });
-
-
-
-
-
--- server/app/app.js
+++ server/app/app.js
@@ -1,10 +1,8 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var exphbs  = require('express3-handlebars');
 var utils = require('connect').utils;
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -17,17 +15,10 @@ mongoose.connect('mongodb://' + connection_string);
 
 var app = express();
 
-// view engine setup
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
-app.set('views', path.join(__dirname, '../../client/views'));
+app.set('views', path.join(__dirname, '../../client'));
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(lessMiddleware(path.join(__dirname, '../../client')));
 app.use(express.static(path.join(__dirname, '../../client')));
 
@@ -38,35 +29,5 @@ app.use(function(req,res,next){
 
 app.use('/', routes);
 app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler: will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler: no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
 
 module.exports = app;
