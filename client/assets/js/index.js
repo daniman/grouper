e6ba3522ddbeef7ee2460e66baf133d567bad5a1
Danielle Man <daniman0808@gmail.com>
2015-03-31 19:01:01 -0400

    removed duplicated files

--- client/assets/js/index.js
+++
@@ -1,7 +0,0 @@
-Handlebars.registerPartial('import_modal', Handlebars.templates['import_modal']);
-Handlebars.registerPartial('nav', Handlebars.templates['nav']);
-
-loadPage = function(template, data) {
-    data = data || {};
-    $('#main-container').html(Handlebars.templates[template](data));
-};
--- server/app/models/user.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var user_schema = mongoose.Schema({
-    username: String,
-    email: String,
-});
-
-var User = mongoose.model('User', user_schema);
-module.exports = User;
