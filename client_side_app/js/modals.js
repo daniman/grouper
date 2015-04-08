6f384d59db26ac078540d51eb454a8d6a134a6b9
Morgan Voss <vossm@mit.edu>
2015-04-08 07:32:46 -0400

    Added student/group modals on double click

--- client_side_app/js/modals.js
+++ client_side_app/js/modals.js
@@ -1,14 +1,53 @@
 $(document).ready(function() {
+
+	//radio buttons for step 3
 	$("#maxPeople").click(function() {
 		$("#numberOfPeople").prop("disabled", false);
+		$("#numberOfGroups").val('');
 		$("#numberOfGroups").prop("disabled", true);
 	});
 	$("#maxGroups").click(function() {
 		$("#numberOfPeople").prop("disabled", true);
+		$("#numberOfPeople").val('');
 		$("#numberOfGroups").prop("disabled", false);
 	});
+
+	//texbox in step 0
+	$('#newGroupName').bind("enterKey",function(e){
+		console.log("wow");
+		$('#importDataModal').modal('show');
+	});
+	
+	$('#newGroupName').keyup(function(e){
+		if(e.keyCode == 13)
+		{
+		  $(this).trigger("enterKey");
+		}
+	});
+
+	//sortable list for priorities
 	$(function() {
-    $( "#sortable" ).sortable();
-    $( "#sortable" ).disableSelection();
-  });
+	    $( "#sortable" ).sortable();
+	    $( "#sortable" ).disableSelection();
+  	});
+
+  	//prevent drag and drop for browser to avoid issues with file drag and drop for step 1
+	$(document).on('dragenter', function (e) 
+	{
+	    e.stopPropagation();
+	    e.preventDefault();
+	});
+
+	$(document).on('dragover', function (e) 
+	{
+	  e.stopPropagation();
+	  e.preventDefault();
+	  obj.css('border', '2px dotted #0B85A1');
+	});
+	$(document).on('drop', function (e) 
+	{
+	    e.stopPropagation();
+	    e.preventDefault();
+	});
+
 });
